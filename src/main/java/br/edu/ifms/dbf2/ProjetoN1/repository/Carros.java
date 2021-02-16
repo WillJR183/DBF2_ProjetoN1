f57ca08287da7cc94b6c4d229289e225295a9849
William Marques <junior.stuart34@gmail.com>
2021-02-16 09:51:33 -0400

    package repository, interface carros adicionados

---
+++ src/main/java/br/edu/ifms/dbf2/ProjetoN1/repository/Carros.java
@@ -0,0 +1,10 @@
+package br.edu.ifms.dbf2.ProjetoN1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.ifms.dbf2.ProjetoN1.model.Carro;
+
+// Interface que contém todos os métodos para realizar a persistencia 
+public interface Carros extends JpaRepository<Carro, Long> {
+	
+}
