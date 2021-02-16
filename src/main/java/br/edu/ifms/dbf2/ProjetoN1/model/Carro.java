b7e6eb8cebc444bfa994c1ed2d2193fe26e7dc6c
William Marques <junior.stuart34@gmail.com>
2021-02-16 09:07:49 -0400

    adicionado MOR carro model, bd h2 configurado

--- src/main/java/br/edu/ifms/dbf2/ProjetoN1/model/Carro.java
+++ src/main/java/br/edu/ifms/dbf2/ProjetoN1/model/Carro.java
@@ -2,21 +2,31 @@ package br.edu.ifms.dbf2.ProjetoN1.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Carro implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue
 	private long id;
 	private String placa;
 	
 	public long getId() {
 		return id;
 	}
+	
 	public void setId(long id) {
 		this.id = id;
 	}
+	
 	public String getPlaca() {
 		return placa;
 	}
+	
 	public void setPlaca(String placa) {
 		this.placa = placa;
 	}
