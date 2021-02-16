beffc952e1b9525c76d75b4ec4b28a43c3406fdf
William Marques <junior.stuart34@gmail.com>
2021-02-16 09:56:15 -0400

    listando carros do BD H2, do backend e frontend

--- src/main/java/br/edu/ifms/dbf2/ProjetoN1/controller/CarroController.java
+++ src/main/java/br/edu/ifms/dbf2/ProjetoN1/controller/CarroController.java
@@ -3,21 +3,30 @@ package br.edu.ifms.dbf2.ProjetoN1.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ifms.dbf2.ProjetoN1.model.Carro;
+import br.edu.ifms.dbf2.ProjetoN1.repository.Carros;
 
 @Controller
 public class CarroController {
 	
+	// objeto que tem a interface Jpa
+	@Autowired
+	private Carros carros;
+		
 	@GetMapping("/carros")
 	public ModelAndView listar() {
 		// ModelAndView - especifica qual view será renderizada
 		ModelAndView modelAndView = new ModelAndView("ListarCarros");
 		// adicionando o bjeto que será manipulado na view
+		// também chamando método para listar dados da lista do backend
 		modelAndView.addObject("carros", buscarCarros());
+		// outro método para listar, porém listando dados do banco de dados H2
+		modelAndView.addObject("carrosBD", carros.findAll());
 		
 		return modelAndView;
 	}
