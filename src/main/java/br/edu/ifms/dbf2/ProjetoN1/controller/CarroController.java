f13c1e329cce7dc1c852376fa5480547258cbfe3
William Marques <junior.stuart34@gmail.com>
2021-02-16 10:26:42 -0400

    carro controller atualizado para realizar CRUD via BD H2

--- src/main/java/br/edu/ifms/dbf2/ProjetoN1/controller/CarroController.java
+++ src/main/java/br/edu/ifms/dbf2/ProjetoN1/controller/CarroController.java
@@ -6,29 +6,55 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ifms.dbf2.ProjetoN1.model.Carro;
-import br.edu.ifms.dbf2.ProjetoN1.repository.Carros;
+import br.edu.ifms.dbf2.ProjetoN1.service.CarroService;
 
 @Controller
 public class CarroController {
 	
-	// objeto que tem a interface Jpa
 	@Autowired
-	private Carros carros;
+	private CarroService carroService;
 		
 	@GetMapping("/carros")
 	public ModelAndView listar() {
+		
 		// ModelAndView - especifica qual view será renderizada
 		ModelAndView modelAndView = new ModelAndView("ListarCarros");
+		
 		// adicionando o bjeto que será manipulado na view
 		// também chamando método para listar dados da lista do backend
 		modelAndView.addObject("carros", buscarCarros());
+		
 		// outro método para listar, porém listando dados do banco de dados H2
 		modelAndView.addObject("carrosBD", carros.findAll());
 		
 		return modelAndView;
+	}
+	
+	@PostMapping
+	public String salvar(Carro carro) {
+		
+		carroService.salvar(carro);
+		return "redirect:/carros";
+	}
+	
+	@GetMapping("/carro/{id}")
+	public ModelAndView editar(@PathVariable("id") Long id) {
+		
+		ModelAndView modelAndView = new ModelAndView("EditarCarro");
+		modelAndView.addObject(carroService.procurar(id));
+		return modelAndView;
+	}
+	
+	@GetMapping("/carro/deletar/{id}")
+	public String deletar(@PathVariable("id") Long id) {
+		
+		carroService.deletar(id);
+		return "redirect:/carros";
 	}
 	
 	public List<Carro> buscarCarros(){
